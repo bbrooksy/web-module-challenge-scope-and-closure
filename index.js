73851e86d85c2a9e4ca7f4445aba35c90c9afbac
bbrooksy <72233725+bbrooksy@users.noreply.github.com>
2021-02-06 12:40:39 -0500

    final changes

--- index.js
+++ index.js
@@ -28,11 +28,14 @@ console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
   Study the code for counter1 and counter2, then answer the questions below.
   
   1. What is the difference between counter1 and counter2?
-  
+  The variable is declared within the function on counter 1 and outside of counter 2.
+
   2. Which of the two uses a closure? How can you tell?
-  
+  counter 1, the } after the end of the initial code
+
   3. In what scenario would the counter1 code be preferable? In what scenario would 
      counter2 be better?  
+     counter2 would allow you to use the 'this' method and able to declare 
 */
 
 // counter1 code
@@ -62,8 +65,8 @@ Use the inning function below to do the following:
 NOTE: This will be a callback function for the tasks below
 */
 
-function inning(/*Code Here*/){
-    /*Code Here*/
+function inning(){
+   return  Math.floor(Math.random()* 3)
 }
 
 
@@ -81,19 +84,32 @@ Use the finalScore function below to do the following:
 }
 */ 
 
-function finalScore(/*code Here*/){
-  /*Code Here*/
+function finalScore(inning, inNum){
+  let score = {
+ "Home": 0,
+ "Away": 0
+};
+for (let i=0; i< inNum; i++){
+ score.Home += inning();
+ score.Away += inning();
 }
+return score;
+}
+
+finalScore(inning, 9);
+
+
 
 /* ⚾️⚾️⚾️ Task 4: getInningScore() ⚾️⚾️⚾️
 Use the getInningScore() function below to do the following:
   1. Receive a callback function - you will pass in the inning function from task 2 as your argument 
   2. Return an object with a score for home and a score for away that populates from invoking the inning callback function */
 
-function getInningScore(/*Your Code Here */) {
-  /*Your Code Here */
+function getInningScore(inning) {
+  let Home = inning();
+  let Away = inning();
+  return {"Home": Home, "Away": Away}
 }
-
 
 /* ⚾️⚾️⚾️ Task 5: scoreboard() ⚾️⚾️⚾️
 Use the scoreboard function below to do the following:
